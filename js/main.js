0f40f3e9ef0b06308abc2f06e8593b6dad3a6a72
rasult22 <warrior.006@inbox.ru>
2019-02-21 14:54:01 +0800

    page

---
+++ js/main.js
@@ -0,0 +1,2 @@
+!function(e){var o=e(".search-toggle"),n=e(".search");e(o).on("click",function(e){n.toggleClass("close")});var t,c=document.querySelector("#grid");imagesLoaded(c,function(){t=new Masonry(c,{itemSelector:".news-item",columnWidth:".news-item",isFitWidth:!0})}),console.log(t)}(jQuery);
+//# sourceMappingURL=main.js.map
